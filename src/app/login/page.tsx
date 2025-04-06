d2a955c279713bfc06bee3e8d8f1253a71bebc69
Akshay.Pandey <n.akshay.pandey@gmail.com>
2025-04-06 03:23:32 -0400

    admin login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useRouter } from 'next/navigation';
+// import { useAuth } from '../context/authContext';
+
+const schema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required'),
+});
+
+export default function Login() {
+//   const { login } = useAuth();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+interface LoginFormInputs {
+    username: string;
+    password: string;
+}
+
+const onSubmit = async (data: LoginFormInputs): Promise<void> => {
+    try {
+        // await login(data.username, data.password);
+        router.push('/dashboard');
+    } catch (error) {
+        alert('Invalid credentials.');
+    }
+};
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-green-50 p-8">
+      <div className="w-full max-w-sm p-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-green-700 text-center mb-6">Admin Login</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <label className="block text-green-800 font-medium mb-1">Username</label>
+            <input
+              type="text"
+              {...register('username')}
+              className="w-full px-4 py-2 border text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
+              placeholder="Enter your username"
+            />
+            {errors.username && <p className="text-red-500 text-sm">{errors.username.message}</p>}
+          </div>
+
+          <div>
+            <label className="block text-green-800 font-medium mb-1">Password</label>
+            <input
+              type="password"
+              {...register('password')}
+              className="w-full px-4 py-2 border rounded-lg text-black  focus:outline-none focus:ring-2 focus:ring-green-400"
+              placeholder="Enter your password"
+            />
+            {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-green-600 hover:bg-green-700 text-white font-bold py-2 rounded-lg transition"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
